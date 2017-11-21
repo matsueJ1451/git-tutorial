b5bc67081648df3373c47041d53a24e7b1585404
Aoto Daichi <j1451@matsue-ct.jp>
2017-11-21 10:13:17 +0900

    create fizzbuzz.rb

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+ in 1..100
+	if i%15 == 0 then
+		printf("FizzBuzz\n")
+	#elsif i%3 == 0 then
+#		printf( "Fizz\n")
+	elsif i%5 == 0 then
+		printf( "Buzz\n")
+	else
+		printf("%d\n", i)
+	end
+end
+
