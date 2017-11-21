2e71686adae42f64afb70aa95ff0020e6a2e1fa2
Aoto Daichi <j1451@matsue-ct.jp>
2017-11-21 10:18:36 +0900

    finished fizzbuss

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
  in 1..100
 	if i%15 == 0 then
 		printf("FizzBuzz\n")
-	#elsif i%3 == 0 then
-#		printf( "Fizz\n")
+	elsif i%3 == 0 then
+		printf( "Fizz\n")
 	elsif i%5 == 0 then
 		printf( "Buzz\n")
 	else
